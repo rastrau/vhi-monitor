93f77c13da46092d8c62de40ef04f1fcd0518f6b
Ralph Straumann <ralph.straumann@hey.com>
2026-07-19 14:47:12 +0300

    Update visualization.R

--- visualization.R
+++ visualization.R
@@ -8,24 +8,52 @@ tilemap_layout <- data.frame(
   code = c(31, 42, 53, 32, 37, 39, 43, 47, 54, 36, 33, 38, 40, 44, 51, 52, 68,
            55, 57, 34, 41, 45, 48, 49, 67, 66, 56, 59, 35, 61, 63, 46, 64, 50,
            58, 60, 62, 65),
-  name = c("Eastern Jura", "Eastern Swiss Plateau", "Franches-Montagnes",
-           "Balsthal", "Oberaargau", "Freiamt", "Lake Zurich Region",
-           "Alpstein", "Western Jura", "Gruyère", "Lower Emmental",
-           "Upper Emmental", "Western Central Switzerland",
-           "Eastern Central Switzerland", "Rhine Valley", "Prättigau",
-           "Lower Engadine", "Western Swiss Plateau", "Chablais",
-           "Bernese Plateau", "Eastern Bernese Oberland", "Uri Alps",
-           "Glarus Alps", "North Central Grisons", "Upper Engadine",
-           "Grisons Southern Valleys", "Lake Geneva Region", "Northern Valais",
-           "Western Bernese Oberland", "Goms", "Northern Ticino", "Surselva",
-           "Eastern Ticino", "South-Central Grisons", "Valais Southern Alps",
-           "Visp Valleys", "Western Ticino", "Sottoceneri"),
+  name = c("Eastern Jura",
+           "Eastern Swiss Plateau",
+           "Franches-Montagnes",
+           "Balsthal",
+           "Oberaargau",
+           "Freiamt",
+           "Lake Zurich\nRegion",
+           "Alpstein",
+           "Western Jura",
+           "Gruyère",
+           "Lower Emmental",
+           "Upper Emmental",
+           "Western Central\nSwitzerland",
+           "Eastern Central\nSwitzerland",
+           "Rhine Valley",
+           "Prättigau",
+           "Lower Engadine",
+           "Western Swiss\nPlateau",
+           "Chablais",
+           "Bernese Plateau",
+           "Eastern Bernese\nOberland",
+           "Uri Alps",
+           "Glarus Alps",
+           "North Central\nGrisons",
+           "Upper Engadine",
+           "Grisons Southern\nValleys",
+           "Lake Geneva\nRegion",
+           "Northern Valais",
+           "Western Bernese\nOberland",
+           "Goms",
+           "Northern Ticino",
+           "Surselva",
+           "Eastern Ticino",
+           "South-Central\nGrisons",
+           "Valais Southern Alps",
+           "Visp Valleys",
+           "Western Ticino",
+           "Sottoceneri"),
   row = c(1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4,
           4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6),
   col = c(4, 7, 3, 4, 5, 6, 7, 8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5,
           6, 7, 8, 9, 10, 1, 3, 4, 5, 6, 7, 8, 9, 4, 5, 7, 8),
   stringsAsFactors = FALSE
 )
+
+region_labels <- setNames(tilemap_layout$name, tilemap_layout$code)
 
 # geofacet::grid_preview(tilemap_layout)
 
@@ -73,14 +101,20 @@ plot_data_daily <- left_join(df_current_year_daily,
                              by = c("region", "day_of_year"))
 
 # Create plot
-plot_daily <- ggplot(plot_data_daily, aes(x = day_of_year)) +
+plot_daily_geo <- ggplot(plot_data_daily, aes(x = day_of_year)) +
   geom_ribbon(aes(ymin = p10, ymax = p90), fill = "orange", alpha = 0.5) +
   geom_line(aes(y = vhi), color = "darkred", linewidth = 1) +
   labs(title = "Current Year vs Historical Percentiles",
        x = "Day of year",
        y = "VHI") +
   theme_minimal() +
-  facet_geo(~ region, grid = tilemap_layout)
+  facet_geo(
+    ~ region,
+    grid = tilemap_layout,
+    labeller = as_labeller(region_labels)
+  )
+
+plot_daily <- wrap_plots(plot_daily_geo, ncol = 1)
 
 # ----------------
 
@@ -98,6 +132,12 @@ df_previous_years_weekly_stats <- df_previous_years_daily %>%
   group_by(region, week_of_year) %>%
   summarise(
     p10 = quantile(vhi, 0.1, na.rm = TRUE),
+    p20 = quantile(vhi, 0.2, na.rm = TRUE),
+    p30 = quantile(vhi, 0.3, na.rm = TRUE),
+    p40 = quantile(vhi, 0.4, na.rm = TRUE),
+    p60 = quantile(vhi, 0.6, na.rm = TRUE),
+    p70 = quantile(vhi, 0.7, na.rm = TRUE),
+    p80 = quantile(vhi, 0.8, na.rm = TRUE),
     p90 = quantile(vhi, 0.9, na.rm = TRUE)
   )
 
@@ -108,24 +148,42 @@ plot_data_weekly <- left_join(df_current_year_weekly,
                               by = c("region", "week_of_year"))
 
 # Create plot
-plot_weekly <- ggplot(plot_data_weekly, aes(x = week_of_year)) +
-  geom_ribbon(aes(ymin = p10, ymax = p90), fill = "orange", alpha = 0.5) +
+plot_weekly_geo <- ggplot(plot_data_weekly, aes(x = week_of_year)) +
+  geom_ribbon(aes(ymin = p10, ymax = p90), fill = "orange", alpha = 0.05) +
+  geom_ribbon(aes(ymin = p20, ymax = p80), fill = "orange", alpha = 0.1) +
+  geom_ribbon(aes(ymin = p30, ymax = p70), fill = "orange", alpha = 0.5) +
+  geom_ribbon(aes(ymin = p40, ymax = p60), fill = "orange", alpha = 0.8) +
   geom_line(aes(y = vhi), color = "darkred", linewidth = 1) +
   labs(title = "Current Year vs Historical Percentiles",
        x = "Week of year",
        y = "VHI") +
   theme_minimal() +
-  facet_geo(~ region, grid = tilemap_layout)
+  facet_geo(
+    ~ region,
+    grid = tilemap_layout,
+    labeller = as_labeller(region_labels)
+  )
 
+plot_weekly_geo <- ggplot(plot_data_weekly, aes(x = week_of_year)) +
+  geom_ribbon(aes(ymin = p30, ymax = p70), fill = "orange", alpha = 0.3) +
+  geom_ribbon(aes(ymin = p40, ymax = p60), fill = "orange", alpha = 0.6) +
+  geom_line(aes(y = vhi), color = "darkred", linewidth = 1) +
+  labs(title = "Current Year vs Historical Percentiles",
+       x = "Week of year",
+       y = "VHI") +
+  theme_minimal() +
+  scale_x_continuous(breaks=c(20, 25, 30),
+                     limits=c(20, 30),
+                     minor_breaks=seq(20, 30, 1)) +
+  facet_geo(
+    ~ region,
+    grid = tilemap_layout,
+    labeller = as_labeller(region_labels)
+  )
 
-plot_daily
-plot_weekly
+plot_weekly <- wrap_plots(plot_weekly_geo, ncol = 1)
 
-plot_daily + plot_weekly + plot_layout(ncol = 1)
-
-plot_daily / plot_weekly
+ggsave("plot.png", plot = plot_weekly, width = 16, height = 12, dpi = 300)
 
 # Clean up connection
 dbDisconnect(con)
-
-
